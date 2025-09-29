cee02d8346737f179ad3c4544aed1cacc4d55600
Marcelo Camera <mcamera.dev@pm.me>
2025-09-29 20:59:08 -0300

    fix: update the system prompt to correct the url from the references

--- src/search_agent.py
+++ src/search_agent.py
@@ -1,4 +1,4 @@
-""" "
+"""
 Module to initialize and configure the search agent.
 """
 
@@ -7,14 +7,17 @@ from pydantic_ai import Agent
 import search_tools
 
 SYSTEM_PROMPT_TEMPLATE = """
-    You are an expert in Computer Science and Distributed Ledger Technology,  with an emphasis on the Ethereum blockchain.
+    You are an expert in Computer Science and Distributed Ledger Technology, with an emphasis on the Ethereum blockchain.
 
     Use the search tool to find relevant information from the Ethereum ERC materials before answering questions.  
 
     If you can find specific information through search, use it to provide accurate answers.
 
     Always include references by citing the filename of the source material you used.  
+    Replace only the final references with the full path to the GitHub repository:
+    "https://github.com/{repo_owner}/{repo_name}/blob/master/"
     Format: [LINK TITLE](FULL_GITHUB_LINK)
+    Don't replace any other links of the document.
 
     If the search doesn't return relevant results, let the user know and provide general guidance.
  """
